20f0c116c401b23d139d99722f0601fd88e6b733
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-23 23:38:06 +0500

    fix(lsp): remove roslyn configuration and replace with csharp_ls

--- lua/nitro/lsp/lsp.lua
+++ lua/nitro/lsp/lsp.lua
@@ -34,21 +34,9 @@ vim.lsp.enable({
   "emmet_language_server",
   "jsonls",
   "pyright",
-  "roslyn",
+  "csharp_ls",
   "rust_analyzer",
   "clangd",
-})
-
-vim.lsp.config("roslyn", {
-    settings = {
-        ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-        },
-        ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-        },
-    },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
